7ec5f0ff004fa96fa8bc0cde6819943eb48c69c8
childressdev <jcampbell@childressagency.com>
2019-03-28 12:08:02 -0400

    removed some comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,3 @@
-//this is from https://coursetro.com/posts/code/130/Learn-Bootstrap-4-Final-in-2018-with-our-Free-Crash-Course
-//another example with gulp: https://github.com/bassjobsen/jbst-4-sass
-
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
@@ -10,7 +7,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 
-// Compile sass into CSS & auto-inject into browsers
+// Compile sass into CSS, minify & auto-inject into browsers
 gulp.task('sass', function(){
   return gulp.src('scss/**/*.scss')
     .pipe(sourcemaps.init())
@@ -22,12 +19,7 @@ gulp.task('sass', function(){
     .pipe(browserSync.stream())
 });
 
-// Move the javascript files into our /src/js folder
-//gulp.task('js', function () {
-//  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-//    .pipe(gulp.dest("js"))
-//    .pipe(browserSync.stream());
-//});
+//concat js, uglify and inject into browser
 gulp.task('js', function(){
   return gulp.src('js/src/*.js')
     .pipe(sourcemaps.init())
@@ -47,12 +39,7 @@ gulp.task('serve', gulp.series('sass', function () {
 
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/**/*.scss'], gulp.series('sass'));
   gulp.watch("*.html").on('change', browserSync.reload);
-  //gulp.watch('js/*.js').on('change', browserSync.reload);
   gulp.watch('js/src/*.js', gulp.series('js'));
 }));
-
-gulp.task('build', function(){
-
-})
 
 gulp.task('default', gulp.parallel(['js','serve']));
